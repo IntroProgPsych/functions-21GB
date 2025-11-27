281766049c858254e797c1978ce64ba9de746566
21GB <ioan.brinzei01@e-uvt.ro>
2025-11-27 09:48:59 +0000

    ok

--- Part1_Functions/3_sum.py
+++ Part1_Functions/3_sum.py
@@ -3,3 +3,14 @@
 # Ask the user for two numbers (as input), convert them to integers, call the function, and print the result.
     
 # Write your code here:
+
+n1 = int(input("Please type in the first number: "))
+n2 = int(input("Please type in the second number: "))
+
+def add(a, b):
+    return a + b
+
+def feedback():
+    print("The sum of ", n1, " and ", n2, " is ", add(n1, n2))
+
+feedback()
--- Part3_Homework/8_Homework.py
+++ Part3_Homework/8_Homework.py
@@ -19,3 +19,24 @@
 # Keep input() outside the functions.
 
 # Write your code here:
+
+score = int(input("Please enter the score: "))
+def calculate_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+    
+def display_report(score, grade):
+    print(f"Score: {score}")
+    print(f"Grade: {grade}")
+
+grade = calculate_grade(score)
+
+display_report(score, grade)
